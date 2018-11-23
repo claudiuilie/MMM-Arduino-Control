c8cc0ce22e522f5ee228d5ce9035aec2f3c057e4
claudiuilie <claudiu.ilie0322@gmail.com>
2018-11-23 22:35:40 +0200

    Arduino-home

--- MMM-Arduino-Control.js
+++ MMM-Arduino-Control.js
@@ -9,13 +9,15 @@
 
 Module.register("MMM-Arduino-Control", {
 	defaults: {
-		updateInterval: 60000,
-		retryDelay: 5000
+		updateInterval: 5000,
+		retryDelay: 5000,
+		animationSpeed: 2000
+
 	},
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function() {
+
+	start: function () {
 		var self = this;
 		var dataRequest = null;
 		var dataNotification = null;
@@ -25,7 +27,7 @@ Module.register("MMM-Arduino-Control", {
 
 		// Schedule update timer.
 		this.getData();
-		setInterval(function() {
+		setInterval(function () {
 			self.updateDom();
 		}, this.config.updateInterval);
 	},
@@ -36,20 +38,21 @@ Module.register("MMM-Arduino-Control", {
 	 * get a URL request
 	 *
 	 */
-	getData: function() {
+	getData: function () {
 		var self = this;
 
-		var urlApi = "https://192.168.1.200" ; // arduino ip
+		var urlApi = "http://192.168.1.200/"; // arduino ip
 		var retry = true;
 
 		var dataRequest = new XMLHttpRequest();
 		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
+		dataRequest.onreadystatechange = function () {
 			console.log(this.readyState);
 			if (this.readyState === 4) {
 				console.log(this.status);
 				if (this.status === 200) {
 					self.processData(JSON.parse(this.response));
+					// console.log(this.response);
 				} else if (this.status === 401) {
 					self.updateDom(self.config.animationSpeed);
 					Log.error(self.name, this.status);
@@ -63,7 +66,7 @@ Module.register("MMM-Arduino-Control", {
 			}
 		};
 		dataRequest.send();
-		// console.log(dataRequest)
+
 	},
 
 
@@ -73,75 +76,75 @@ Module.register("MMM-Arduino-Control", {
 	 * argument delay number - Milliseconds before next update.
 	 *  If empty, this.config.updateInterval is used.
 	 */
-	scheduleUpdate: function(delay) {
+	scheduleUpdate: function (delay) {
 		var nextLoad = this.config.updateInterval;
 		if (typeof delay !== "undefined" && delay >= 0) {
 			nextLoad = delay;
 		}
-		nextLoad = nextLoad ;
+		nextLoad = nextLoad;
 		var self = this;
-		setTimeout(function() {
+		setTimeout(function () {
 			self.getData();
 		}, nextLoad);
 	},
 
-	getDom: function() {
+	getDom: function () {
 		var self = this;
 
 		// create element wrapper for show into the module
+
 		var wrapper = document.createElement("div");
-		// If this.dataRequest is not empty
+		wrapper.className = "home-assistant";
+		wrapper.innerHTML = "ssss";
 		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
-			// check format https://jsonplaceholder.typicode.com/posts/1
-			wrapperDataRequest.innerHTML = this.dataRequest.title;
 
-			var labelDataRequest = document.createElement("label");
-			// Use translate function
-			//             this id defined in translations files
-			labelDataRequest.innerHTML = this.translate("TITLE");
+			var homeassistantLegend = document.createElement("legend");
+			homeassistantLegend.className = "module-header";
+			homeassistantLegend.innerHTML = "Home Assistant";
+			wrapper.appendChild(homeassistantLegend);
+
+			var sensors = document.createElement("fieldset");
+			sensors.className = "sensors-assistant";
+			wrapper.appendChild(sensors);
+
+			var sensorsLegend = document.createElement("legend");
+			sensorsLegend.innerHTML = "Sensors";
+			sensors.appendChild(sensorsLegend);
+			for (var i = 0; i < this.dataRequest.tempSensors.length; i++) {
+				console.log(this.dataRequest.tempSensors[i].badge)
 
 
-			wrapper.appendChild(labelDataRequest);
-			wrapper.appendChild(wrapperDataRequest);
+				sensors.insertAdjacentHTML('beforeend', `${this.dataRequest.tempSensors[i].badge}<strong id="bedroomTempValue"><span class="badge bedroom">${this.dataRequest.tempSensors[i].temp} ${this.dataRequest.tempSensors[i].unit}</span></strong></br >`)
+			}
+			var relays = document.createElement("fieldset");
+			relays.className = "relays-assistant";
+
+			var relaysLegend = document.createElement("legend");
+			relaysLegend.innerHTML = this.dataRequest.relayStatus.bedroom.status;
+			relays.appendChild(relaysLegend);
+			wrapper.appendChild(relays);
 		}
 
-		// Data from helper
-		if (this.dataNotification) {
-			var wrapperDataNotification = document.createElement("div");
-			// translations  + datanotification
-			wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
 
-			wrapper.appendChild(wrapperDataNotification);
-		}
+
 		return wrapper;
 	},
 
-	getScripts: function() {
-		return [];
-	},
 
 	getStyles: function () {
 		return [
 			"MMM-Arduino-Control.css",
 		];
 	},
-
-	// Load translations files
-	getTranslations: function() {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json"
-		};
-	},
-
-	processData: function(data) {
+	processData: function (data) {
 		var self = this;
 		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
+		console.log(data);
+		console.log(this.dataRequest)
+		if (this.loaded === false) { self.updateDom(self, this.config.animationSpeed); }
+		//<------------aici crapa
 		this.loaded = true;
-
+		this.show(this.config.animationSpeed, { lockString: this.identifier });
 		// the data if load
 		// send notification to helper
 		this.sendSocketNotification("MMM-Arduino-Control-NOTIFICATION_TEST", data);
@@ -149,8 +152,10 @@ Module.register("MMM-Arduino-Control", {
 
 	// socketNotificationReceived from helper
 	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MMM-Arduino-Control-NOTIFICATION_TEST") {
+		console.log(notification);
+		if (notification === "MMM-Arduino-Control-NOTIFICATION_TEST") {
 			// set dataNotification
+
 			this.dataNotification = payload;
 			this.updateDom();
 		}
